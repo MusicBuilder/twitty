0b87245f1b32f1217727c01634b3837b91aaf0a8
MusicBuilder <Musicbuilder01@gmail.com>
2016-09-08 10:15:56 -0400

    ainitial cut

--- db.js
+++ db.js
@@ -6,21 +6,35 @@ var db = new sqlite3.Database('twitty.db');
 
 function initDB(db) {
     db.serialize(function() {
-        db.run("CREATE TABLE lorem (info TEXT)");
+   //     db.run("DROP TABLE tweets", function(err){ if (err) {}});
+        db.run("CREATE TABLE tweets (AUTHOR INT NOT NULL,MESSAGE CHAR(140) NOT NULL,TS TEXT NOT NULL)", function(err){ if (err) {}});
 
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-        for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
-        }
+        var stmt = db.prepare("INSERT INTO tweets VALUES (1,'test', '9/8/2016')");
+        stmt.run();
         stmt.finalize();
 
-        db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+        db.each("SELECT AUTHOR, MESSAGE, TS FROM tweets", function (err, row) {
             if (err) {
                 console.log(err);
             }
-            console.log(row.id + ": " + row.info);
+            console.log(row.AUTHOR + ": " + row.MESSAGE + " " + row.TS);
+        });
+    });
+    db.serialize(function() {
+     //  db.run("DROP TABLE users", function(err){ if (err) {}}); //x
+        db.run("CREATE TABLE users (USERID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL)", function(err){ if (err) {}});
+
+        var stmt = db.prepare("INSERT INTO users VALUES (1, 'Brian')");
+        stmt.run();
+        stmt.finalize();
+
+        db.each("SELECT USERID, NAME FROM users", function (err, row) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(row.USERID + ": " + row.NAME);
         });
     });
 }
-
+initDB(db);
 // db.close();
